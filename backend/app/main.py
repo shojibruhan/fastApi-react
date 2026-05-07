025cc7f063da33dd221f1995b58aab28172c6dec
Shojib H. Ruhan <si.ruhan09@gmail.com>
2026-05-08 03:08:59 +0600

    create endpoint
    - GET endpoint
    - POST endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,43 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+class Fruit(BaseModel):
+    name: str
+
+class FruitResponse(BaseModel):
+    fruits: List[Fruit]
+
+origins= [
+    "http://localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_methods= ['*'],
+    allow_headers= ['*']
+)
+
+db= {
+    "fruits": []
+}
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/fruits")
+def create_fruit(fruit: Fruit):
+    db["fruits"].append(fruit)
+    return fruit
+
+@app.get("/fruits", response_model= FruitResponse)
+def get_fruits():
+    return FruitResponse(fruits= db["fruits"])
 
